e0202d7c3dba605e4c149efa3f9f70734bb40a04
BobbyShaftoe <tech@halcyonnetworks.com.au>
2018-03-20 09:56:16 +0800

    Update elastic spark R script

--- R/elastic_spark_05.R
+++ R/elastic_spark_05.R
@@ -27,16 +27,17 @@ es <- connect(es_host = elastic_search_host)
 es_json <- Search(index = es_index, type = es_type, raw = T, size = 2000)
 es_list <- jsonlite::fromJSON(es_json, flatten = T, simplifyDataFrame = T, simplifyVector = T, simplifyMatrix = T)
 es_df <- as_data_frame(es_list$hits$hits)
-es_df <- es_df[c(- 1, - 2, - 3, - 4, - 5)]
-
+es_df <- es_df[c(-1,-2,-3,-4,-5)]
 
 # FORMAT COLUMN NAMES CLEANLY
 es_df_names <- colnames(es_df) %>%
-    gsub(pattern = '\\.|:', replacement = '_') %>%
-    gsub(pattern = '^_source_', replacement = '_') %>%
-    gsub(pattern = '^_', replacement = '') %>%
-    gsub(pattern = '-', replacement = '_')
+  gsub(pattern = '\\.|:', replacement = '_') %>%
+  gsub(pattern = '^_source_', replacement = '_') %>%
+  gsub(pattern = '^_', replacement = '') %>%
+#  gsub(pattern = '^', replacement = 'o_') %>%
+  gsub(pattern = '-', replacement = '_')
 colnames(es_df) <- es_df_names
+#es_df <- as.data.frame(es_df)
 
 
 # UNNEST LIST COLUMNS WITH FUNCTION
@@ -51,36 +52,36 @@ es_tmp <- as.data.frame(es_df)
 
 # FUNCTION TO SELECT LIST COLUMNS AND UNNEST THEM CLEANLY
 func_selective_unnest <- function(tmp_df, column_names) {
-    df <- tmp_df
-    for (column_name in column_names) {
-        print(column_name)
+  df <- tmp_df
+  for (column_name in column_names) {
+    print(column_name)
 
-        # GENERATE LOGICAL VECTOR BY TYPE FOR COLUMN BEING OPERATED ON
-        t <- mapply(FUN = is.data.frame, df[, paste0(column_name)], SIMPLIFY = F)
-        # SELECT ONLY THOSE COLUMNS OF TYPE
-        f <- df[t == "TRUE",]
-        # UNNEST THEM
-        g <- unnest_(f, column_name, .drop = F, .id = column_name)
+    # GENERATE LOGICAL VECTOR BY TYPE FOR COLUMN BEING OPERATED ON
+    t <- mapply(FUN = is.data.frame, df[, paste0(column_name)], SIMPLIFY = F)
+    # SELECT ONLY THOSE COLUMNS OF TYPE
+    f <- df[t == "TRUE",]
+    # UNNEST THEM
+    g <- unnest_(f, column_name, .drop = F, .id = column_name)
 
-        # REMOVE ORIGINAL NESTED COLUMN IF NEW COLUMNS WERE CREATED
-        if (ncol(g) > ncol(df)) {
-            df <- select(df, - starts_with(column_name))
-            g <- select(g, - starts_with(column_name))
-            # GET NEW COLUMN NAMES AND ADD TO SOURCE DF AS NEW COLS FILLED WITH NAs
-            cols_to_add <- setdiff(colnames(g), colnames(df))
-            df[, cols_to_add] <- NA
-            # ADD THE UNNESTED ROWS TO ORIGINAL DF
-            df <- rbind(df, g)
-            # APPEND SOURCE COLUMN NAME TO NAMES OF NEW COLS
-            cols_renamed <- str_replace_all(cols_to_add, "^", paste0(column_name, "."))
-            setnames(df, old = cols_to_add, new = cols_renamed)
-        }
+    # REMOVE ORIGINAL NESTED COLUMN IF NEW COLUMNS WERE CREATED
+    if (ncol(g) > ncol(df)) {
+      df <- select(df, -starts_with(column_name))
+      g <- select(g, -starts_with(column_name))
+      # GET NEW COLUMN NAMES AND ADD TO SOURCE DF AS NEW COLS FILLED WITH NAs
+      cols_to_add <- setdiff(colnames(g), colnames(df))
+      df[, cols_to_add] <- NA
+      # ADD THE UNNESTED ROWS TO ORIGINAL DF
+      df <- rbind(df, g)
+      # APPEND SOURCE COLUMN NAME TO NAMES OF NEW COLS
+      cols_renamed <- str_replace_all(cols_to_add, "^", paste0(column_name, "."))
+      setnames(df, old = cols_to_add, new = cols_renamed)
     }
-    return(df)
+  }
+  return(df)
 }
 # UNNEST THE LIST COLUMNS CLEANLY
 es_new_df <- tbl_df(func_selective_unnest(es_tmp, es_list_cols))
-es_new_df <- es_new_df[, order(names(es_new_df))]
+es_new_df <- es_new_df[,order(names(es_new_df))]
 
 View(tail(es_new_df, n = 50))
 
@@ -91,10 +92,10 @@ event_names <- as.list(unique(es_new_df$detail_eventName))
 es_dfs <- list()
 # CREATE NAMED LIST OF DATAFRAMES BY EVENT NAMES
 func_make_event_df <- function(event_name, df){
-    tmp_df <- filter(df, detail_eventName == event_name) %>% select_if(~ sum(! is.na(.)) > 0)
-    return(tmp_df)
-}
-es_dfs <- lapply(setNames(event_names, event_names), function(x) func_make_event_df(x, es_new_df))
+  tmp_df <- filter(df, detail_eventName == event_name) %>% select_if(~sum(!is.na(.)) > 0)
+  return(tmp_df)
+  }
+es_dfs <- lapply(setNames(event_names,event_names), function(x) func_make_event_df(x, es_new_df))
 
 
 
@@ -104,18 +105,33 @@ df_tmp <- es_dfs[["Decrypt"]]
 
 # FUNCTION TO REMOVE EMPTY COLUMNS IN DATAFRAME
 func_remove_null_cols <- function(df){
-    df[is.null(df)] <- NA
-    df <- df %>% replace(. == "NA", NA)
-    df <- df %>% replace(. == "NULL", NA)
-    df <- df %>% replace(. == "", NA)
+  df[is.null(df)] <- NA
+  df <- df %>% replace(.=="NA", NA)
+  df <- df %>% replace(.=="NULL", NA)
+  df <- df %>% replace(.=="", NA)
     emptycols <- sapply(df, function (k) all(is.na(k)))
-    return(df[! emptycols])
+  return(df[!emptycols])
 }
 df_tmp <- func_remove_null_cols(df_tmp)
 
 
 
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+########################## UTILITY FUNCTIONS ##########################
 
+# INSPECT NULL/NA/EMPTY VALUES FUNCTION
+tmp <- df_tmp$detail_userIdentity_accessKeyId
+
+sapply(tmp, function(x)
+  paste0("variable: \"", x, "\",  length: (", length(x), "), class: [", class(x), "], match: (", grepl("^.+", x), ")")) %>%
+  unique() %>%
+  View(title="Match Valid")
+
+gp <- sapply(tmp, function(x) grepl("^.+", x))
+ga <- sapply(tmp, function(x) grepl("^$", x))
+
+View(unique(tmp[gp]))
+View(unique(tmp[!gp]))
 
 
 #############  WORK WITH VALUES TO FACTORS ############
@@ -132,8 +148,8 @@ df_tmp <- func_remove_null_cols(df_tmp)
 
 
 ### CONVERT ISO8601 DATE TIME FORMAT
-tmp <- as.POSIXct(gsub("Z$", "", df_tmp$time), "%Y-%m-%dT%H:%M:%S", tz = "GMT")
-df_tmp$time <- as.POSIXct(gsub("Z$", "", df_tmp$time), "%Y-%m-%dT%H:%M:%S", tz = "UTC")
+tmp <- as.POSIXct(gsub("Z$", "", df_tmp$time), "%Y-%m-%dT%H:%M:%S", tz="GMT")
+df_tmp$time <- as.POSIXct(gsub("Z$", "", df_tmp$time), "%Y-%m-%dT%H:%M:%S", tz="UTC")
 
 # ARRANGE BY  DATE
 df_tmp <- df_tmp %>% arrange(time)
@@ -142,43 +158,11 @@ df_tmp <- df_tmp %>% arrange(time)
 
 # GROUP AND SUMMARISE BY FIELDS
 #df_tmp <- df_tmp %>% group_by(time, detail_sourceIPAddress) %>%
-df_tmp <- df_tmp %>%
-    group_by(time, detail_userIdentity_accessKeyId, detail_sourceIPAddress) %>%
+df_tmp <- df_tmp %>% group_by(time, detail_userIdentity_accessKeyId, detail_sourceIPAddress) %>%
     summarise(count = n())
 
 View(df_tmp)
 hist(tmp, breaks = "hours", freq = T, col = "gold")
-
-
-
-
-
-
-########################################################################################################
-############################################ UTILITY FUNCTIONS #########################################
-
-# INSPECT NULL/NA/EMPTY VALUES FUNCTION
-
-# ASSIGN A LIST OR VECTOR TO 'TMP' VARIABLE
-tmp <- df_tmp$detail_userIdentity_accessKeyId
-
-sapply(tmp, function(x)
-paste0("variable: \"", x, "\",  length: (", length(x), "), class: [", class(x), "], match: (", grepl("^.+", x), ")")) %>%
-    unique() %>%
-    View(title = "Valid Type Matches")
-
-gp <- sapply(tmp, function(x) grepl("^.+", x))
-ga <- sapply(tmp, function(x) grepl("^$", x))
-
-View(unique(tmp[gp]))
-View(unique(tmp[! gp]))
-
-########################################################################################################
-
-
-
-
-
 
 
 
@@ -203,7 +187,9 @@ View(unique(tmp[! gp]))
 # tmp <- xts(es_dfs[["StartInstances"]], order.by = as.Date(es_dfs[["StartInstances"]]$time))
 
 
+
 #tmp <- func_remove_null_cols(filter(es_new_df, !is.na(detail_userIdentity_accessKeyId)))
+
 
 
 # what are the most common issues that cause an error to be logged?
